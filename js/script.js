60afe471b294debfa47385bbccd6d5ff8cd5e4e2
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-13 15:15:18 +0100

    milestone 0 completed

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const team = [
+    {
+        nome: "Wayne",
+        ruolo: "Barnett",
+        foto: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg",
+    }
+]
